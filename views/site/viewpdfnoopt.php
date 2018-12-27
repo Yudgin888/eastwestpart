86ad3b8c7b1c8238c6511e9a5402f53060622afc
Евгений Пинчук <E.Pinchuk@webcom.lan>
2018-12-27 18:16:32 +0300

    27.12

---
+++ views/site/viewpdfnoopt.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: E.Pinchuk
+ * Date: 27.12.2018
+ * Time: 14:52
+ */
---
+++ views/site/viewpdfopt.php
@@ -0,0 +1,14 @@
+<?php
+
+$this->title = 'Создание КП | Модель';
+?>
+<div class="site-index">
+    <div class="body-content">
+
+        <div class="pdf-content">
+
+
+        </div>
+
+    </div>
+</div>
